cced19bffc3e483f3b49bc57b064048e37216293
山本有紗 <Arisa@s-arisa-yamamotonomacbook-pro.local>
2017-11-09 20:07:36 +0900

    画像丸くしたよ

--- wanwan/GraphViewController.swift
+++ wanwan/GraphViewController.swift
@@ -13,9 +13,44 @@ class GraphViewController: UIViewController, UICollectionViewDataSource {
     @IBOutlet var collectionView:UICollectionView!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         collectionView.dataSource = self
+        // UIImage インスタンスの生成
+        let image:UIImage = UIImage(named:"chiko1.jpg")!
+        
+        // UIImageView 初期化
+        let imageView = UIImageView(image:image)
+        
+        // 画面の横幅を取得
+        let screenWidth:CGFloat = view.frame.size.width/2
+        let screenHeight:CGFloat = view.frame.size.height/2
+        
+        // 画像の幅・高さの取得
+        let imgWidth = image.size.width
+        let imgHeight = image.size.height
+        
+        // 画像サイズをスクリーン幅に合わせる
+        let scale = screenWidth / imgWidth * 0.9
+        let rect:CGRect = CGRect(x:0, y:0, width:imgWidth*scale, height:imgHeight*scale)
+        
+        // ImageView frame をCGRectで作った矩形に合わせる
+        imageView.frame = rect;
+        
+        // 画像の中心を画面の中心に設定
+        imageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+        
+        // 角丸にする
+        imageView.layer.cornerRadius = imageView.frame.size.width * 0.1
+        imageView.clipsToBounds = true
+        
+        // UIImageViewのインスタンスをビューに追加
+        self.view.addSubview(imageView)
+        
+        //　画像をさらに５０％丸くする
+        imageView.layer.cornerRadius = imageView.frame.size.width * 0.5
+
         
 
         // Do any additional setup after loading the view.
@@ -38,6 +73,9 @@ class GraphViewController: UIViewController, UICollectionViewDataSource {
     }
     
     
+}
+    
+    
     
 
     /*
@@ -50,4 +88,4 @@ class GraphViewController: UIViewController, UICollectionViewDataSource {
     }
     */
 
-}
+
