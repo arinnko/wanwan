e93491a659efb85f2b4d1c40740d2b0ed6a71a57
家 <arisa@yamamotoarisanomacbook-puro.local>
2018-10-25 20:27:58 +0900

    たくさんやったよ

---
+++ wanwan/DailyViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DailyViewController.swift
+//  wanwan
+//
+//  Created by 山本有紗 on 2018/10/25.
+//  Copyright © 2018年 山本有紗. All rights reserved.
+//
+
+import UIKit
+
+class DailyViewController: UIViewController{
+}
